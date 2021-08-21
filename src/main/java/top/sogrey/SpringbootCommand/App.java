8dd5d0bf62cb31c971c9e2d14c9ac61471ad0c97
Sogrey <suzhaoheng2006@163.com>
2021-08-21 17:15:52 +0800

    add command-cli

---
+++ src/main/java/top/sogrey/SpringbootCommand/App.java
@@ -0,0 +1,36 @@
+package top.sogrey.SpringbootCommand;
+
+import java.util.Scanner;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	try {
+            System.out.println("*****************************************");
+            System.out.println("*                                       *");
+            System.out.println("*     版本 V1.0.0\t\t\t*");
+            System.out.println("*                                       *");
+            System.out.println("*     1 选项一\t\t\t\t*");
+            System.out.println("*     2 选项二\t\t\t\t*");
+            System.out.println("*     0 退出程序\t\t\t*");
+            System.out.println("*                                       *");
+            System.out.println("*****************************************");
+            boolean flag = true;
+            while (flag) {
+                Scanner input = new Scanner(System.in);
+                System.out.println();
+                System.out.print("请输入Code：");
+                String a = input.nextLine();
+                System.out.println("输入："+a);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
